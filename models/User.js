4b2ceeaac3cba2ecdc87e8f69442884cb19aa83d
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-10-28 16:24:27 -0400

    edited userRoutes.js in api folder

--- models/User.js
+++ models/User.js
@@ -28,7 +28,7 @@ const userSchema = new Schema(
   },
   {
     // Mongoose supports two Schema options to transform Objects after querying MongoDb: toJSON and toObject.
-    // Here we are indicating that we want virtuals to be included with our response, overriding the default behavior
+    // Here we are indicating that we want virtuals to be included with our reaction, overriding the default behavior
     toJSON: {
       virtuals: true,
     },
