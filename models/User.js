0c258adbcdc8d555139777269b3d48d948ca60c7
monica dolce <106848108+monicadolce@users.noreply.github.com>
2022-10-28 14:20:25 -0400

    updated User.js with email validation

--- models/User.js
+++ models/User.js
@@ -1,11 +1,18 @@
 const { Schema, model } = require('mongoose');
 
+const validateEmail = (email) => {
+    const re = /^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/;
+    return re.test(email);
+};
+
 // Schema to create User model
 const userSchema = new Schema(
   {
-    first: String,
-    last: String,
-    age: Number,
+    username: { type: String, unique: true, required: true, trimmed: true }, 
+    email: { type: String, required: true, unique: true,
+        validate: [validateEmail, 'Enter a valid email address'],
+        match: [/^([a-z0-9_\.-]+)@([\da-z\.-]+)\.([a-z\.]{2,6})$/, 'Enter a valid email address'],
+         },
     thoughts: [
       {
         type: Schema.Types.ObjectId,
@@ -31,17 +38,17 @@ const userSchema = new Schema(
 
 // Create a virtual property `fullName` that gets and sets the user's full name
 userSchema
-  .virtual('fullName')
+  .virtual('friendCount')
   // Getter
   .get(function () {
-    return `${this.first} ${this.last}`;
-  })
-  // Setter to set the first and last name
-  .set(function (v) {
-    const first = v.split(' ')[0];
-    const last = v.split(' ')[1];
-    this.set({ first, last });
+    return this.friends.length;
   });
+  // Setter to set the first and last name
+//   .set(function (v) {
+//     const first = v.split(' ')[0];
+//     const last = v.split(' ')[1];
+//     this.set({ first, last });
+//   });
 
 // Initialize our User model
 const User = model('user', userSchema);
